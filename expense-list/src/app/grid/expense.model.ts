a73bebb0b2d421f1fc3536f029923a5840065af6
Merlin <merlin@umaknow.com>
2016-10-29 22:28:35 -0400

    Fix load date (merge two expense list) load provider et taxohidden list

--- expense-list/src/app/grid/expense.model.ts
+++ expense-list/src/app/grid/expense.model.ts
@@ -1,17 +1,21 @@
 export class Expense {
-  id:number;
-  title : string;
+  id: number;
+  title: string;
   filename?: string;
   price: number;
   validated: boolean;
-  manager : string; //TODO trouver un meilleur nom
-  date : string; //TODO pas un string...
-  year:number;
-  authorId:number;
-  created:string;
-  modified:string;
-  providerId;
-  flat:string;
-  taxCategory:string;
-  relativeEditLink:string;
+  manager: string; // TODO trouver un meilleur nom
+  date: string; // TODO pas un string...
+  year: number;
+  authorId: number;
+  created: string;
+  modified: string;
+  providerId: number;
+  provider: string;
+  flatId: string;
+  flat: string;
+  taxCategoryId: string;
+  taxCategory: string;
+  relativeEditLink: string;
+  type: string;
 }
--- expense-list/src/app/grid/grid.component.ts
+++ expense-list/src/app/grid/grid.component.ts
@@ -3,9 +3,9 @@ import {Component, OnInit} from '@angular/core';
 import pnp from 'sp-pnp-js';
 import * as _ from 'lodash';
 import {GridOptions} from 'ag-grid/main';
-import {Expense} from './expense.model'
-import {Provider} from "./provider.model";
-import {TaxonomyHiddenList} from "./taxonomyHiddenList.model";
+import {Expense} from './expense.model';
+import {Provider} from './provider.model';
+import {TaxonomyHiddenList} from './taxonomyHiddenList.model';
 
 @Component({
   selector: 'app-grid',
@@ -15,8 +15,8 @@ import {TaxonomyHiddenList} from "./taxonomyHiddenList.model";
 export class GridComponent implements OnInit {
   expenses: [Expense] = [];
   rowData: [Expense] = [];
-  providers : [Provider] = [];
-  taxonomyHiddenList : [TaxonomyHiddenList] = [];
+  providers: [Provider] = [];
+  taxonomyHiddenList: [TaxonomyHiddenList] = [];
   depenseListItems: any;
   depenseListDocuments: any;
   private gridOptions: GridOptions;
@@ -27,13 +27,15 @@ export class GridComponent implements OnInit {
     this.gridOptions = <GridOptions>{};
     this.columnDefs = [
       {headerName: 'Action', template: '<div style="width: 100%" data-action-type="openItem">Ouvrir</div>', width: 80},
+      {headerName: 'Type', field: 'type', width: 50},
       {headerName: 'Id', field: 'id', width: 50},
       {headerName: 'Prix', field: 'price', width: 100},
+      {headerName: 'Titre', field: 'title', width: 100},
       {headerName: 'Validé', field: 'validated', width: 100},
       {headerName: 'Gestionnaire', field: 'manager', width: 100},
       {headerName: 'Date', field: 'date', width: 100},
       {headerName: 'Année', field: 'year', width: 100},
-      {headerName: 'Fournisseur', field: 'providerId', width: 100},
+      {headerName: 'Fournisseur', field: 'provider', width: 100},
       {headerName: 'Logement', field: 'flat', width: 100},
       {headerName: 'Catégorie de taxe', field: 'taxCategory', width: 100},
     ];
@@ -44,29 +46,34 @@ export class GridComponent implements OnInit {
     let batch = pnp.sp.createBatch();
     this.gridOptions.rowData = this.rowData;
     pnp.sp.web.lists.getByTitle('Depenses').items.top(10000).inBatch(batch).get().then(res => {
-      console.log('Documents');
       this.depenseListItems = res;
       _.map(this.depenseListItems, item => {
         let x = new Expense;
+        x.type = 'Document';
         x.price = item.Prix;
         x.validated = item.Valide;
         x.id = item.Id;
         x.created = item.Created;
         x.modified = item.Modified;
         x.date = item.Date1;
-        //x.year = item.Date.getFullYear();
+
         x.authorId = item.AuthorId;
         x.providerId = item.FournisseursId;
         x.title = item.Title;
+        x.manager = item.GestionnairesChoice;
         if (item.odata) {
           x.relativeEditLink = item.odata.editLink;
         }
-        x.manager = item.GestionnairesChoice;
+
+        if (x.date != undefined) {
+          x.year = parseInt(x.date.substr(0, 4));
+        }
+
         if (item.Logements) {
-          x.flat = item.Logements.Label;
+          x.flatId = item.Logements.Label;
         }
         if (item.TaxesCategory) {
-          x.taxCategory = item.TaxesCategory.Label;
+          x.taxCategoryId = item.TaxesCategory.Label;
         }
 
 
@@ -76,39 +83,38 @@ export class GridComponent implements OnInit {
       if (this.rowData.length > 1) {
         this.rowData.concat(this.expenses);
       } else {
-        this.rowData = this.expenses
+        this.rowData = this.expenses;
       }
-
-
-      console.log('this.Expenses');
-      console.log(this.expenses);
     });
-    pnp.sp.web.lists.getByTitle('Dépenses').items.top(10000).inBatch(batch).get().then(res => {
-      console.log('Lists');
+    pnp.sp.web.lists.getByTitle('D%C3%A9penses').items.top(10000).inBatch(batch).get().then(res => {
       this.depenseListDocuments = res;
-
       _.map(this.depenseListDocuments, item => {
         let x = new Expense;
+        x.type = 'item';
         x.price = item.Montant;
         x.validated = item.Valid_x00e9_;
         x.id = item.Id;
         x.created = item.Created;
         x.modified = item.Modified;
         x.date = item.Date;
-        //x.year = date.getYear();
         x.authorId = item.AuthorId;
         x.providerId = item.FournisseursId;
         x.title = item.Title;
+
+
+        if (x.date != undefined) {
+          x.year = parseInt(x.date.substr(0, 4));
+        }
         if (item.odata) {
           x.relativeEditLink = item.odata.editLink;
         }
         x.manager = item.GestionnairesChoice;
 
         if (item.Logements) {
-          x.flat = item.Logements.Label;
+          x.flatId = item.Logements.Label;
         }
         if (item.TaxesCategory) {
-          x.taxCategory = item.TaxesCategory.Label;
+          x.taxCategoryId = item.TaxesCategory.Label;
         }
 
 
@@ -118,10 +124,8 @@ export class GridComponent implements OnInit {
       if (this.rowData.length > 1) {
         this.rowData.concat(this.expenses);
       } else {
-        this.rowData = this.expenses
+        this.rowData = this.expenses;
       }
-      console.log(this.expenses);
-
     });
     pnp.sp.site.rootWeb.lists.getByTitle('Fournisseurs').items.top(5000).inBatch(batch).get().then(res => {
       _.map(res, item => {
@@ -130,10 +134,9 @@ export class GridComponent implements OnInit {
         x.title = item.Title;
         this.providers.push(x);
       });
-    console.log(this.providers)
+      console.log(this.providers)
     });
     pnp.sp.site.rootWeb.lists.getByTitle('TaxonomyHiddenList').items.top(5000).inBatch(batch).get().then(res => {
-      console.log(res);
       _.map(res, item => {
         let x = new TaxonomyHiddenList;
         x.id = item.Id;
@@ -143,20 +146,45 @@ export class GridComponent implements OnInit {
         x.term1036 = item.Term1036;
         this.taxonomyHiddenList.push(x);
       });
-    })
-    batch.execute().then(function() {
-
+      console.log(this.taxonomyHiddenList);
+    });
+    batch.execute().then(() => {
+      console.log('everything is loaded !!!');
+      _.map(this.expenses, (expenseItem) => {
+        let taxoItemFiltered = _.filter(this.taxonomyHiddenList, (taxoItem) => {
+          return taxoItem.id == expenseItem.flatId;
+        });
+        if (taxoItemFiltered.length > 0) {
+          expenseItem.flat = taxoItemFiltered[0].term1036;
+        }
+      });
+      _.map(this.expenses, (expenseItem) => {
+        let taxoItemFiltered = _.filter(this.taxonomyHiddenList, (taxoItem) => {
+          return taxoItem.id == expenseItem.taxCategoryId;
+        });
+        if (taxoItemFiltered.length > 0) {
+          expenseItem.taxCategory = taxoItemFiltered[0].term1036;
+        }
+      });
+      _.map(this.expenses, (expenseItem) => {
+        let providerItemFiltered = _.filter(this.providers, (providerItem) => {
+          return providerItem.id == expenseItem.providerId;
+        });
+        console.log(providerItemFiltered);
+        if (providerItemFiltered.length > 0) {
+          expenseItem.provider = providerItemFiltered[0].title;
+        }
+      });
     });
 
-
-  }
+  };
 
   private calculateRowCount() {
 
     if (this.gridOptions.api && this.rowData) {
-      var model = this.gridOptions.api.getModel();
-      var totalRows = this.rowData.length;
-      var processedRows = model.getRowCount();
+      let model = this.gridOptions.api.getModel();
+      let totalRows = this.rowData.length;
+      let processedRows = model.getRowCount();
       this.rowCount = processedRows.toLocaleString() + ' / ' + totalRows.toLocaleString();
     }
   }
@@ -179,7 +207,7 @@ export class GridComponent implements OnInit {
     // console.log('onRowClicked: ' + $event.node.data.name);
     if ($event.event.target !== undefined) {
       let data = $event.data;
-      //TODO PB - let actionType = $event.event.target.getAttribute("data-action-type");
+      // TODO PB - let actionType = $event.event.target.getAttribute("data-action-type");
       return this.openItem(data);
 
       /*            switch (actionType) {
@@ -191,7 +219,7 @@ export class GridComponent implements OnInit {
 
   public openItem(data: any) {
     // console.log("View action clicked", data);
-    //this.router.navigate(['engagement', {currentID: data.notInModel.Id}]);
+    // this.router.navigate(['engagement', {currentID: data.notInModel.Id}]);
   }
 
   private onQuickFilterChanged($event) {
